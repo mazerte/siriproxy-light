c5563048401e9b62152d2ae21c0496f9f4eb147f
mazerte <mathieu.desve@me.com>
2013-02-03 06:13:14 +0100

    add thread

--- lib/siriproxy-light.rb
+++ lib/siriproxy-light.rb
@@ -7,7 +7,7 @@ class SiriProxy::Plugin::Light < SiriProxy::Plugin
     @config = config 
   end
 
-  listen_for /(allume|éteins) la lampe (.+)/i do |action, color|
+  listen_for /(allume|éteins) la lampe (\w+)/i do |action, color|
     case action
     when "allume"
       result = "allumé"
@@ -24,8 +24,11 @@ class SiriProxy::Plugin::Light < SiriProxy::Plugin
       led = "red"
     end
     
-    page = HTTParty.get("http://rpi.home:1313/push?led=#{led}&state=#{state}").body rescue nil
-    say "J'ai #{result} la lampe #{color}", spoken: "http://rpi.home:1313/push?led=#{led}&state=#{state}"
+    Thread.new {
+      page = HTTParty.get("http://rpi.home:1313/push?led=#{led}&state=#{state}").body rescue nil
+      say "J'ai #{result} la lampe #{color}"
+      request_completed
+    }
   end
   
 end
